feb15ab79147689ed43748adb3aa47fa3ec57c27
anusreesjith <anusreesjith@gmail.com>
2026-02-28 16:42:06 +0530

    feat: add skeleton structure

---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+      <input className="search-input" type="text"></input>
+      <div className="search-suggestions"></div>
+    </div>
+  );
+};
+
+export default Search;
